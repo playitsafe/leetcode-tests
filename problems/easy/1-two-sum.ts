c87d1c1221049a118a21f89bf1d36bc5fc42a783
aaron.zhang <aaron.zhang@wanews.com.au>
2023-05-13 17:42:58 +0800

    init with 1-two-sum

---
+++ problems/easy/1-two-sum.ts
@@ -0,0 +1,25 @@
+// 
+// function twoSum(nums: number[], target: number): number[] {
+//     const lengthToLoop = nums.length - 1
+//     for (let i = 0; i < lengthToLoop; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if ((nums[i] + nums[j]) === target) {
+//                 return [i, j]
+//             }
+//         }
+//     }
+//     return []
+// };
+
+function twoSum(nums: number[], target: number): number[] {
+    const numberMap = new Map()
+    for (const [i, num] of nums.entries()) {
+        const numInMap = numberMap.get(target - num)
+        if (numInMap != null) {
+            return [numInMap, i]
+        } else {
+            numberMap.set(num, i)
+        }
+    }
+    return []
+}
