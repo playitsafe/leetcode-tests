aed8560677f3b630779809cd3f0082c069dae26f
aaron.zhang <aaron.zhang@wanews.com.au>
2023-05-19 02:07:02 +0800

    add solution

--- problems/easy/13-roman-to-int/13-roman-to-int.test.ts
+++ problems/easy/13-roman-to-int/13-roman-to-int.test.ts
@@ -1,5 +1,11 @@
 import { romanToInt } from './13-roman-to-int'
 
-test('should convert roman to integer', () => {
-    
+test('should convert III to 3', () => {
+    expect(romanToInt('M')).toEqual(1000)
+    expect(romanToInt('III')).toEqual(3)
+    expect(romanToInt('XII')).toEqual(12)
+    expect(romanToInt('XXVII')).toEqual(27)
+    expect(romanToInt('LVIII')).toEqual(58)
+    expect(romanToInt('MCM')).toEqual(1900)
+    expect(romanToInt('MCMXCIV')).toEqual(1994)
 })
--- problems/easy/13-roman-to-int/13-roman-to-int.ts
+++ problems/easy/13-roman-to-int/13-roman-to-int.ts
@@ -1,5 +1,38 @@
 // https://leetcode.com/problems/roman-to-integer/
-export function romanToInt(s: string): number {
+const numMap = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+} as const
 
-    return 0
+type RomanLetter = keyof typeof numMap
+
+export function romanToInt(s: string): number {
+    let total = 0
+    const len = s.length
+    for (let i = 0; i < len; i++) {
+        const letter = s[i] as RomanLetter
+        const num = numMap[letter]
+
+        const nextLetter = s[i+1] as RomanLetter
+        const nextNum = numMap[nextLetter]
+
+        if (num < nextNum) {
+            total -= num
+        } else {
+            total += num
+        }
+    }
+
+    return total
+};
+
+// Todo - investigate
+function romanToInt2(s: string): number {
+    const numbers: number[] = s.split('').map(v => numMap[v as RomanLetter]);
+    return numbers.reduce((acc, num, index) => num < numbers[index + 1] ?? 0 ? acc - num : acc + num, 0);
 };
