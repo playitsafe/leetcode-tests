e620d3adc64ecf9315a4b87f460be8d5ea855c32
playitsafe <playitsafe@qq.com>
2023-05-28 01:08:31 +0800

    add more  solution

--- problems/easy/26-remove-duplicates-from-sorted-array/index.ts
+++ problems/easy/26-remove-duplicates-from-sorted-array/index.ts
@@ -10,3 +10,34 @@ export function removeDuplicates(nums: number[]): number {
 
   return nums.length - adjust
 }
+
+function removeDuplicates1(nums: number[]): number {
+  if (nums.length === 1) return 1
+
+  let p2 = 0
+  let p1 = 0
+
+  while (p2 < nums.length) {
+    if (nums[p1] < nums[p2]) {
+      nums[p1 + 1] = nums[p2]
+      p1++
+    }
+    p2++
+  }
+  return p1 + 1
+}
+
+function removeDuplicates2(nums: number[]): number {
+  let prev: any = nums[0]
+  let prevIdx: number = 1
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== prev) {
+      nums[prevIdx] = nums[i]
+      prevIdx++
+      prev = nums[i]
+    }
+  }
+
+  return prevIdx
+}
