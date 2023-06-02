fd649e90598cc7c8fc1e4d6ff5b3155004e6c756
playitsafe <playitsafe@qq.com>
2023-06-03 04:05:33 +0800

    add solution

--- problems/easy/28-find-index-of-the-first-occurrence-in-a-string/index.ts
+++ problems/easy/28-find-index-of-the-first-occurrence-in-a-string/index.ts
@@ -1,1 +1,15 @@
-export function strStr(haystack: string, needle: string): number {}
+export function strStr(haystack: string, needle: string): number {
+  //   if (!haystack.includes(needle)) return -1
+
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack.startsWith(needle, i)) {
+      return i
+    }
+  }
+
+  return -1
+}
+
+export function strStr1(haystack: string, needle: string): number {
+  return haystack.indexOf(needle)
+}
