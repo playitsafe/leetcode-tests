bfc02b1cc654fb27b325e32e1dcb1bb0d02653a1
Aaron <37096803+playitsafe@users.noreply.github.com>
2023-06-10 02:15:00 +0800

    Fix-66:plus one (#12)
    add fix

--- problems/easy/66-plus-1/index.test.ts
+++ problems/easy/66-plus-1/index.test.ts
@@ -5,6 +5,9 @@ const arr2 = [4, 3, 2, 1]
 const arr3 = [4, 3, 9, 9]
 const arr4 = [9]
 const arr5 = [9, 9, 9, 9]
+const arr6 = [1]
+const arr7 = [8, 9, 9, 9]
+const arr8 = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]
 
 it('toString approach shall pass', () => {
   expect(plusOne(arr1)).toEqual([1, 2, 4])
@@ -12,6 +15,10 @@ it('toString approach shall pass', () => {
   expect(plusOne(arr3)).toEqual([4, 4, 0, 0])
   expect(plusOne(arr4)).toEqual([1, 0])
   expect(plusOne(arr5)).toEqual([1, 0, 0, 0, 0])
+  expect(plusOne(arr6)).toEqual([2])
+  expect(plusOne(arr7)).toEqual([9, 0, 0, 0])
+  // this will fail first solution
+  expect(plusOne(arr8)).toEqual([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 4])
 })
 
 it('recursive pop approach shall pass', () => {
@@ -20,4 +27,7 @@ it('recursive pop approach shall pass', () => {
   expect(plusOne1(arr3)).toEqual([4, 4, 0, 0])
   expect(plusOne1(arr4)).toEqual([1, 0])
   expect(plusOne1(arr5)).toEqual([1, 0, 0, 0, 0])
+  expect(plusOne1(arr6)).toEqual([2])
+  expect(plusOne1(arr7)).toEqual([9, 0, 0, 0])
+  expect(plusOne1(arr8)).toEqual([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 4])
 })
--- problems/easy/66-plus-1/index.ts
+++ problems/easy/66-plus-1/index.ts
@@ -1,24 +1,19 @@
 const { log: l } = console
 
-// to string
+// number to string
 export function plusOne(digits: number[]): number[] {
   const nums = +digits.join('') + 1
   return Array.from(nums.toString(), Number)
 }
 
-// array pop
+// recursive plus
 export function plusOne1(digits: number[], tail: 0[] = []): number[] {
   const lastDigi = digits.pop()
   if (lastDigi === 9) {
     tail.push(0)
     return plusOne1(digits, tail)
   } else {
-    if (digits.length > 0) {
-      digits.push(lastDigi! + 1)
-    } else {
-      digits.push(1)
-    }
-
+    digits.push((lastDigi || 0) + 1)
     // Note: concat() will return a new array
     return digits.concat(tail)
   }
